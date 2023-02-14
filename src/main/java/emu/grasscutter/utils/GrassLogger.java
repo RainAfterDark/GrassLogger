ee9eb477f05c7ef0b9a4021b4059758f8ce5059a
雨 <42061128+RainAfterDark@users.noreply.github.com>
2023-02-15 03:49:54 +0800

    Update GrassLogger.java

--- src/main/java/emu/grasscutter/utils/GrassLogger.java
+++ src/main/java/emu/grasscutter/utils/GrassLogger.java
@@ -272,6 +272,7 @@ public class GrassLogger {
             Float.toString(attackResult.getElementDurabilityAttenuation()),
             getElementName(element.getValue()),
             getReaction(aid, mid, element, getRoot(attackerId)),
+                                                          //getAmplifyReactionType
             AmplificationType.getTypeByValue(attackResult.getUnk3300ABJJNNNKGBB()).toString(),
             Float.toString(attackResult.getElementAmplifyRate()),
             Integer.toString(attackResult.getAttackCount()),
